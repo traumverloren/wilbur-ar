b54ac4b9d5b9a9736718036f519438ba48c12522
Stephanie Nemeth <traumverloren@gmail.com>
2019-11-24 11:08:08 +0100

    Initial Commit

---
+++ wilbur-ar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wilbur-ar
+//
+//  Created by Steph on 11/24/19.
+//  Copyright © 2019 Steph. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
